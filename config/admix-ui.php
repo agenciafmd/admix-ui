13599833273d07381b6e824f76b7bd5333496db0
Irineu Junior <irineu@fmd.ag>
2024-01-30 08:15:49 -0300

    feat(command) Criar o comando de atualizar o tabler

--- config/admix-ui.php
+++ config/admix-ui.php
@@ -2,4 +2,9 @@
 
 return [
     'prefix' => '',
+    // https://github.com/tabler/tabler/releases
+    'tabler' => [
+        'path' => 'vendor/agenciafmd/admix-ui',
+        'version' => 'v1.0.0-beta20',
+    ],
 ];
--- src/Console/Commands/UiUpdateCommand.php
+++ src/Console/Commands/UiUpdateCommand.php
@@ -3,18 +3,30 @@
 namespace Agenciafmd\Ui\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Process\Pipe;
+use Illuminate\Support\Facades\Process;
 
 class UiUpdateCommand extends Command
 {
-    protected $signature = 'admix:ui-update {--tag=dev : Versão do tabler que será instalada}';
+    protected $signature = 'admix:ui-update {--tag= : Versão do tabler que será instalada}';
 
     protected $description = 'Atualiza o tabler para a versão desejada';
 
     public function handle(): int
     {
-        $this->line('');
-        $this->components->error('Implementar / Olhar o admix:install');
+        $tag = $this->option('tag') ?? config('admix-ui.tabler.version');
 
-        return static::SUCCESS;
+        $this->components->info('Atualizando o tabler para a versão ' . $tag);
+
+        $result = Process::pipe(static function (Pipe $pipe) use ($tag) {
+            $pipe->path(config('admix-ui.tabler.path'))
+                ->command('rm -rf tabler');
+            $pipe->path(config('admix-ui.tabler.path'))
+                ->command('git clone --depth 1 --branch ' . $tag . ' git@github.com:tabler/tabler.git');
+            $pipe->path(config('admix-ui.tabler.path'))
+                ->command('rm -rf tabler/.git');
+        });
+
+        return $result->successful() ? static::SUCCESS : static::FAILURE;
     }
 }
