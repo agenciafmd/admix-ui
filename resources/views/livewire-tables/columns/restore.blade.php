4d2097e06e532b3a43f650ee1095c5659a054c25
Irineu Junior <irineu@fmd.ag>
2024-04-19 15:04:17 -0300

    feat(role) devolve o botão de restaurar

---
+++ resources/views/livewire-tables/columns/restore.blade.php
@@ -0,0 +1,21 @@
+<button x-data="{}"
+        x-on:click="{{ $path }}"
+        {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!}
+>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         class="icon d-sm-none d-block m-0"
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         stroke-width="2"
+         stroke="currentColor"
+         fill="none"
+         stroke-linecap="round"
+         stroke-linejoin="round"
+    >
+        <use xlink:href="{{ asset('vendor/admix/images/tabler-sprite.svg') }}#tabler-restore"/>
+    </svg>
+    <span class="d-none d-sm-block">
+        {{ $title }}
+    </span>
+</button>
